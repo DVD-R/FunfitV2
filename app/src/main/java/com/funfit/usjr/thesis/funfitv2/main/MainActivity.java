227a4ff3d3f07e7f31b33bbfa161be2a3d489c64
andalesv <victorandalezz@gmail.com>
2016-01-06 03:20:45 +0800

    Main Activity and Health Fragment

---
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.funfit.usjr.thesis.funfitv2.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.funfit.usjr.thesis.funfitv2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

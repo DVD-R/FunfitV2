5192932581354a741089e97c24cab6208aa6def9
andalesv <victorandalezz@gmail.com>
2016-01-07 01:17:48 +0800

    refinement of health pref design

--- app/src/main/java/com/funfit/usjr/thesis/funfitv2/healthPreference/HealthPreferenceActivity.java
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/healthPreference/HealthPreferenceActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.funfit.usjr.thesis.funfitv2.R;
 import com.funfit.usjr.thesis.funfitv2.listener.LeftGestureListener;
 import com.funfit.usjr.thesis.funfitv2.listener.RightGestureListener;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -18,13 +21,14 @@ import butterknife.OnClick;
  */
 public class HealthPreferenceActivity extends AppCompatActivity {
 
-
+    @Bind(R.id.heightSpnr)Spinner mHeightSpinner;
     GestureDetectorCompat gestureDetectorCompat;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.health_preference_activity);
 
+        ButterKnife.bind(this);
         gestureDetectorCompat = new GestureDetectorCompat(this, new RightGestureListener(this));
     }
 
@@ -32,5 +36,9 @@ public class HealthPreferenceActivity extends AppCompatActivity {
     public boolean onTouchEvent(MotionEvent event) {
         this.gestureDetectorCompat.onTouchEvent(event);
         return super.onTouchEvent(event);
+    }
+
+    public void click(View view){
+        Toast.makeText(this,mHeightSpinner.getSelectedItem().toString(),Toast.LENGTH_LONG).show();
     }
 }

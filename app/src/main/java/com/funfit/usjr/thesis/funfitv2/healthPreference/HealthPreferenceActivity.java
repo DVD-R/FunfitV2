54ba4241e1c57931619337c4dabcbbbd8c26cf51
andalesv <victorandalezz@gmail.com>
2016-01-07 01:38:51 +0800

    update

--- app/src/main/java/com/funfit/usjr/thesis/funfitv2/healthPreference/HealthPreferenceActivity.java
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/healthPreference/HealthPreferenceActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -30,6 +31,18 @@ public class HealthPreferenceActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         gestureDetectorCompat = new GestureDetectorCompat(this, new RightGestureListener(this));
+
+        mHeightSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override

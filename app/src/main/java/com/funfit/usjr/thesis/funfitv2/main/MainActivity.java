5e5cb6abeda62ac7a07f92d5ce82656adfed0fb4
andalesv <victorandalezz@gmail.com>
2016-01-07 02:25:24 +0800

    fixing conflict

--- app/src/main/java/com/funfit/usjr/thesis/funfitv2/main/MainActivity.java
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/main/MainActivity.java
@@ -1,87 +1,68 @@
 package com.funfit.usjr.thesis.funfitv2.main;
 
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.funfit.usjr.thesis.funfitv2.R;
+import com.funfit.usjr.thesis.funfitv2.mealPlan.MealPlanActivity;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DrawerLayout drawerLayout;
     Toolbar toolbar;
+
+    @Bind(R.id.drawer_layout)DrawerLayout mDrawerLayout;
+    @Bind(R.id.toolbar)Toolbar mToolBar;
+    @Bind(R.id.navigation)
+    NavigationView navigationView;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private final Handler mDrawerActionHandler = new Handler();
+    private static final long DRAWER_CLOSE_DELAY_MS = 250;
+    private static final String NAV_ITEM_ID = "navItemId";
+    private int mNavItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupNavigationView();
-        setupToolbar();
+        ButterKnife.bind(this);
+        navigationView.setNavigationItemSelectedListener(this);
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar, R.string.open,
+                R.string.close);
+
+
+        if (null == savedInstanceState) {
+//            mNavItemId = R.id.drawer_item_1;
+        } else {
+            mNavItemId = savedInstanceState.getInt(NAV_ITEM_ID);
+        }
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                if(drawerLayout != null)
-                    drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new MealPlanActivity()).commit();
+
     }
 
-    private void setupNavigationView(){
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    }
-
-    private void setupToolbar(){
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        if(toolbar != null)
-            setSupportActionBar(toolbar);
-
-        // Show menu icon
-        final ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
-    }
-
-    /*This method initialize the position of navigation drawer items*/
-    /*after you click in navigation drawer icon it return integer position and focus a tab by the use of setCurrentItem()*/
-    private void navigate(final int itemId) {
-        switch (itemId) {
-            case R.id.nav_health_pref:
-                break;
-            case R.id.nav_meal_archeive:
-                break;
-            case R.id.nav_food_description:
-                break;
-            case R.id.nav_challenge:
-                break;
-            case R.id.nav_weekly:
-                break;
-            case R.id.nav_history:
-                break;
-            case R.id.nav_event:
-                break;
-        }
-    }
-
-    /*if you click specific item in navigation drawer it automatically close the navigation drawer*/
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        drawerLayout.closeDrawers();
-                        navigate(menuItem.getItemId());
-                        return true;
-                    }
-                });
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        return false;
     }
 }

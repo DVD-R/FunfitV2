06e1111937c476cf42afdf587aa85857a4a8237b
andalesv <victorandalezz@gmail.com>
2016-01-08 18:08:06 +0800

    login background

--- app/src/main/java/com/funfit/usjr/thesis/funfitv2/login/LoginActivity.java
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/login/LoginActivity.java
@@ -1,24 +1,33 @@
 package com.funfit.usjr.thesis.funfitv2.login;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.funfit.usjr.thesis.funfitv2.R;
 import com.funfit.usjr.thesis.funfitv2.healthPreference.HealthPreferenceActivity;
 import com.funfit.usjr.thesis.funfitv2.main.MainActivity;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
+    @Bind(R.id.img_login_bg)ImageView mImageBg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        Glide.with(this)
+                .load(Uri.parse("http://djunabel.com/images/pics/554_djuna-bel-nike-db8.jpg"))
+                .centerCrop()
+                .into(mImageBg);
     }
 
     @OnClick(R.id.facebookBtn)

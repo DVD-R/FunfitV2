b9e32ff03ca8607e924955a0170c70e9f3c82fd3
ocabafox <ocabaa123>
2016-01-13 01:02:03 +0800

    Distance Calculation

---
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/distance/DistanceCalculation.java
@@ -0,0 +1,39 @@
+package com.funfit.usjr.thesis.funfitv2.distance;
+
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by ocabafox on 1/12/2016.
+ */
+public class DistanceCalculation {
+
+
+    public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+        Log.i("Radius", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec);
+
+        return Radius * c;
+    }
+}

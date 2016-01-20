f0d0002597b4842b94f9348dac0a63a4117971d4
Dj <denj.rey@gmail.com>
2016-01-21 02:47:54 +0800

    Notification Layout

--- app/src/main/java/com/funfit/usjr/thesis/funfitv2/notificationEvents/NotificationActivity.java
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/notificationEvents/NotificationActivity.java
@@ -1,11 +1,18 @@
 package com.funfit.usjr.thesis.funfitv2.notificationEvents;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.funfit.usjr.thesis.funfitv2.R;
+import com.funfit.usjr.thesis.funfitv2.model.Notification;
+import com.funfit.usjr.thesis.funfitv2.viewmods.DividerItemDecoration;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,13 +23,70 @@ import butterknife.ButterKnife;
 public class NotificationActivity extends AppCompatActivity{
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
+    @Bind(R.id.recycler_notification)
+    RecyclerView mRecyclerView;
+
+    //Notification types
+    private static final int NOTIFICATION_ENEMY_ENVADE = 0;
+    private static final int NOTIFICATION_ALLY_FORTIFIED = 1;
+
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected LayoutManagerType mCurrentLayoutManagerType;
+    protected NotificationAdapter mAdapter;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        //Activity Setup
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+
+        fetchNotifications();
+    }
+
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+
+
+    //This method is currently for mock purposes
+    private void fetchNotifications() {
+        ArrayList<Notification> list = new ArrayList<Notification>();
+
+        list.add(new Notification(NOTIFICATION_ENEMY_ENVADE,"ViviDub",
+                "http://36.media.tumblr.com/6f6ee1b0efdcd68b0b42b3926b1813a8/tumblr_inline_ntoq4xTqIk1rzdmz3_400.jpg", null));
+        list.add(new Notification(NOTIFICATION_ALLY_FORTIFIED,"MightyElmo",
+                "http://data.whicdn.com/images/1274695/large.jpg", null));
+        list.add(new Notification(NOTIFICATION_ALLY_FORTIFIED,"Sammmmmy",
+                "http://41.media.tumblr.com/tumblr_m8d2cfhiG11rd0j11o1_r1_1280.jpg", null));
+        list.add(new Notification(NOTIFICATION_ENEMY_ENVADE,"FatCat",
+                "https://33.media.tumblr.com/a277c5fc90d5ec52b3b0122769ba9fed/tumblr_inline_n1ve4lGOBw1reu5mf.jpg", null));
+        list.add(new Notification(NOTIFICATION_ALLY_FORTIFIED,"WowDoge",
+                "http://i.imgur.com/z5k5ykt.jpg", null));
+
+        mAdapter = new NotificationAdapter(list);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/notificationEvents/NotificationAdapter.java
@@ -0,0 +1,133 @@
+package com.funfit.usjr.thesis.funfitv2.notificationEvents;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.funfit.usjr.thesis.funfitv2.R;
+import com.funfit.usjr.thesis.funfitv2.model.Notification;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Dj on 1/20/2016.
+ */
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder>{
+    ArrayList<Notification> mList;
+
+    //Notification types
+    private static final int NOTIFICATION_ENEMY_ENVADE = 0;
+    private static final int NOTIFICATION_ALLY_FORTIFIED = 1;
+
+    //temporary var for time
+    private int time=2;
+
+    private static ColorStateList ally;
+    private static ColorStateList enemy;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        @Bind(R.id.img_playertwo)
+        ImageView mImagePlayerTwo;
+        @Bind(R.id.img_status)
+        ImageView mImageStatus;
+        @Bind(R.id.text_info)
+        TextView mTextInfo;
+        @Bind(R.id.text_timestamp)
+        TextView mTextTime;
+
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            ally = ContextCompat.getColorStateList(itemView.getContext(),R.color.velocity);
+            enemy = ContextCompat.getColorStateList(itemView.getContext(),R.color.impulse);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public NotificationAdapter(ArrayList<Notification> list){
+        this.mList = list;
+    }
+
+    @Override
+    public NotificationAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_notification, parent, false);
+        v.setFocusable(true);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Notification data = mList.get(position);
+        String name = data.getPlayerTwoUsername();
+
+        Glide.with(holder.mImagePlayerTwo.getContext())
+                .load(data.getPlayerTwoImgUrl())
+                .centerCrop()
+                .crossFade()
+                .into(holder.mImagePlayerTwo);
+        holder.mImageStatus.setImageResource(getStatusIcon(data.getNotificationType()));
+        holder.mTextInfo.setText(spanText(data.getNotificationType(),
+                name.length(), name + infoMessage(data.getNotificationType())));
+        holder.mTextTime.setText(time++ + " hours ago");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    private int getStatusIcon(int notificationType) {
+        switch (notificationType){
+            case NOTIFICATION_ALLY_FORTIFIED: return R.drawable.status_blue_shield;
+            case NOTIFICATION_ENEMY_ENVADE: return R.drawable.status_red_sword;
+        }
+        return 0;
+    }
+
+    private String infoMessage(int notificationType) {
+        switch (notificationType){
+            case NOTIFICATION_ALLY_FORTIFIED: return " has fortified your territory.";
+            case NOTIFICATION_ENEMY_ENVADE: return " has envaded your territory.";
+        }
+        return null;
+    }
+
+    private Spannable spanText(int notificationType, int nameLength, String text) {
+        Spannable span = new SpannableString(text);
+        StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
+
+        switch (notificationType){
+            case NOTIFICATION_ALLY_FORTIFIED: span.setSpan(new ForegroundColorSpan(ally.getDefaultColor()), 0,
+                    nameLength, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); break;
+            case NOTIFICATION_ENEMY_ENVADE: span.setSpan(new ForegroundColorSpan(enemy.getDefaultColor()), 0,
+                    nameLength, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); break;
+        }
+
+        span.setSpan(bss, 0, nameLength, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        return span;
+    }
+
+}

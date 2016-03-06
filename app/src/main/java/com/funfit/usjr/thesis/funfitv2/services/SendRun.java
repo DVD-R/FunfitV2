f92d0f248200f58cad11a3bd556ded688a72b57d
ocabafox <ocabaa123>
2016-03-06 22:56:46 +0800

    Distance and Time

---
+++ app/src/main/java/com/funfit/usjr/thesis/funfitv2/services/SendRun.java
@@ -0,0 +1,35 @@
+package com.funfit.usjr.thesis.funfitv2.services;
+
+/**
+ * Created by ocabafox on 3/6/2016.
+ */
+public class SendRun {
+
+    private double time;
+    private double distance;
+    private double calories;
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+}
